8398875ffab1a909be285b26dfaa2ce2449f74b8
tcltere <t.canette@gmail.com>
2018-01-02 20:25:07 -0300

    trabajos recuperaativos

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,8 +1,10 @@
 //rotar elemento a la derecha ejercicio 1
 function rotarElementos(array, n){
  for(i=0; i< n; i++){ //se recorre el arreglo con el for
-   var elemente = array.pop(); //.pop elimina el ultimo array .
+   var elements = array.pop(); //.pop elimina el ultimo array .
    array.unshift(elements) //Agrega elementos al inicio.
  };
  return array;
 };
+var arr = [1, 2, 3, 4, 5, 6, 7];
+rotarElementos(arr, 3);
--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -3,7 +3,7 @@
 function buscar (name){
   var name = prompt("Ingresa el nombre");//con el prompt pido que ingrese un mombre y lo guardo en esta var
   var list = ["Allison", "Belén", "Nadia", "Marcia", "Ale", "Fabián"] 
-  for(var i = 0; i < lista.length; i++){//recorro el arreglo con for
+  for(var i = 0; i < list.length; i++){//recorro el arreglo con for
     if(list[i] === name) {//si i es igual igual al nombre ingresado le da posicion
       return i+1;
   }
